39492ccfd476b8f2e9d32c1c7ba5889b3db9481e
Semira-Umer <semiraoumer769@gmail.com>
2025-09-11 08:43:55 -0700

    initiall commit

--- great.js
+++ great.js
@@ -2,3 +2,12 @@ function submit() {
  const userName = prompt("Hi Please enter your name");
  alert("Hello " +userName+ " Welcome to your to do list 😊");
 };
+//create  an object named task
+let task={
+    name: "semira",
+    completed: false,
+    id: r14rty,
+}
+console.log(typeof task.name);//string
+console.log(typeof task.completed);//boolean
+console.log(typeof task.id);//number
